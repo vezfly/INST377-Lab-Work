f62a6ff99eb37f62ca0329f5cac45abd42d9d7d2
vezfly <vez@terpmail.umd.edu>
2022-03-19 12:50:39 -0400

    finished lab 6

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,41 +1,43 @@
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 function dataHandler(array) {
-  const button = document.querySelector('button');
+  const listItems = document.querySelector('#resto-list');
+  listItems.innerHTML = ""
+  let newArray = [...Array(15).keys()]
+  let arrayMap = newArray.map((elm) => { 
+    randomInt =  getRandomIntInclusive(0,array.length)
+    return array[randomInt]
+  })
 
-  button.addEventListener("button", display => {
-    button.style.setProperty(display:"none")
+  arrayMap.forEach((elm) => {
+    const str = `<li>${elm.name}</li>`
+    listItems.innerHTML +=str
+  })
 
-    // ^ NOT SURE IF THIS RIGHT
-
-  });
-
-  const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
-  const arrayFromJson = await results.json(); // This changes it into data we can use - an object
-
-  array => console.table(arrayFromJson.data); // this is called "dot notation"
-    // arrayFromJson.data - we're accessing a key called 'data' on the returned object
-    // it contains all 1,000 records we need
-
-    // ^ NOT SURE IF THIS RIGHT
 }
 
 async function mainEvent() { // the async keyword means we can make API requests
   const form = document.querySelector('.main_form');
-  form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
-    submitEvent.preventDefault(); // This prevents your page from refreshing!
-    console.log('form submission'); // this is substituting for a "breakpoint"
-  });
+  const button = document.querySelector('button');
+  button.style.display = 'none';
+  const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
+  const arrayFromJson = await results.json(); // This changes it into data we can use - an object
+
+    if(arrayFromJson.data.length > 0){
+      button.style.display = 'block'
+
+      form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
+        submitEvent.preventDefault(); // This prevents your page from refreshing!
+        console.log('form submission'); // this is substituting for a "breakpoint"
+
+        dataHandler(arrayFromJson.data)
+      });
+  }
 }
 
-dataHandler(); // < NOT SURE IF THIS RIGHT
-
-
-if(array.length() > 0){
-  // this actually runs first! It's calling the function above
-  document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
-
-  button.addEventListener("button", display => {
-    button.style.setProperty(display:"block")
-
-    // ^ NOT SURE IF THIS RIGHT
-  });
-}
+// this actually runs first! It's calling the function above
+document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
